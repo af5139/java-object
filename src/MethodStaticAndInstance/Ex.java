1c4e7ee9ef301564fa9e954343042cff521f9cea
woong <gi5060@naver.com>
2022-09-23 14:12:46 +0900

    static method practice

---
+++ src/MethodStaticAndInstance/Ex.java
@@ -0,0 +1,21 @@
+package MethodStaticAndInstance;
+
+public class Ex {
+    int i;
+    int j;
+    int k=5;
+    static int si;
+    public int add(){
+        si=3;
+        System.out.println(si);//클래스 변수 사용할 수 있음
+        System.out.println(k);//인스턴스 변수 사용할 수 있음
+        return i+j;
+    }
+
+    public static int add(int i,int j){//static 에서는 static만 사용 가능
+    //    System.out.println(k); 인스턴스 변수를 사용할 수 없음
+        si=5;
+        System.out.println(si);
+        return i+j;
+    }
+}
---
+++ src/MethodStaticAndInstance/Main.java
@@ -0,0 +1,14 @@
+package MethodStaticAndInstance;
+
+public class Main {
+    public static void main(String[] args) {
+        //객체 생성
+        Ex ex = new Ex();
+        ex.i=3;
+        ex.j=5;
+
+        System.out.println(ex.add());
+
+        System.out.println(Ex.add(3,5));
+    }
+}
